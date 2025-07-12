5130e6d7e4c6e1c16085542bd292018b9fb97148
Swathi Nallamothu <swathichinnarao1@gmail.com>
2025-07-12 12:06:31 +0530

    Add files via upload

---
+++ append.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+file=open("text.txt","w")
+file.write("have a nice day")
+file.close()
+
+
+# In[11]:
+
+
+file=open("text.txt","a")
+file.write("123")
+file.close()
+
+
+# In[ ]:
+
+
+
+
